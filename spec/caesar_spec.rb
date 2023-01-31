cdaac22f31b73273cf0de13b668d47561b7db03f
Kasper <kasperbak89@pm.me>
2023-01-31 15:56:58 +0100

    create test for multiple letters

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,7 +1,11 @@
 require "./lib/caesar.rb"
 
 describe "#ceaser_cipher" do
-  it "shifts a single letter up the alphabet by the number of the passed argument" do
+  it "shifts a single letter up the alphabet" do
     expect(ceaser_cipher('d', 3)).to eql('g')
+  end
+
+  it "shifts multiple letters up the alphabet" do
+    expect(ceaser_cipher('lmn', 5)).to eql('qrs')
   end
 end
