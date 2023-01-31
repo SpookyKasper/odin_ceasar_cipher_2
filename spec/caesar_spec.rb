bc1c3ba8b4ba4e976908a324aa603463ba058578
Kasper <kasperbak89@pm.me>
2023-01-31 16:07:13 +0100

    create 4 more tests

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -8,4 +8,20 @@ describe "#ceaser_cipher" do
   it "shifts multiple letters up the alphabet" do
     expect(ceaser_cipher('lmn', 5)).to eql('qrs')
   end
+
+  it "loops when alphabet end" do
+    expect(ceaser_cipher('xyz', 3)).to eql('abc')
+  end
+
+  it "deals with mixed case strings" do
+    expect(ceaser_cipher('XyZ', 3)).to eql('AbC')
+  end
+
+  it "deals with spaces and punctuation" do
+    expect(ceaser_cipher('xYz! yes', 3)).to eql('aBc! bhv')
+  end
+
+  it "deals with an empty string" do
+    expect(ceaser_cipher('', 3)).to eql('')
+  end
 end
